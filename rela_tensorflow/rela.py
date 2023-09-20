51194bcab5ccff1e960e55a26b7a0523e2e83c65
Armand <72499921+brandnewchoppa@users.noreply.github.com>
2023-09-20 09:04:45 +0200

    model replacement

--- rela_tensorflow/rela.py
+++ rela_tensorflow/rela.py
@@ -20,7 +20,7 @@ Pytorch implementation of ReLA (https://github.com/lucidrains/rela-transformer)
 '''
 
 class GatedRMSNorm(Layer):
-    """
+    '''
     Gated Root Mean Square Layer Normalization (GatedRMSNorm)
     https://arxiv.org/pdf/2104.07012.pdf
 
@@ -32,7 +32,7 @@ class GatedRMSNorm(Layer):
     https://github.com/lucidrains/rela-transformer (logic)
     https://github.com/bzhangGo/rmsnorm (logic)
     https://github.com/google-deepmind/dm-haiku/blob/main/haiku/_src/rms_norm.py (logic)
-    """
+    '''
 
     def __init__(self,
                  *,
@@ -69,7 +69,7 @@ class GatedRMSNorm(Layer):
         return tf.nn.sigmoid(x * self.w) * rms
     
 class ReLA(Layer):
-    """
+    '''
     Rectified Linear Attention (ReLA)
     https://arxiv.org/pdf/2104.07012.pdf
 
@@ -77,7 +77,7 @@ class ReLA(Layer):
 
     References:
     https://arxiv.org/pdf/1907.01470.pdf (memory)
-    """
+    '''
     
     def __init__(self,
                  causal : bool = True,
@@ -125,111 +125,47 @@ class ReLA(Layer):
         x = tf.reshape(out, [b, n, self.qkv_dim])
         return self.to_out(x)
 
-class PositionEmbedding(tf.keras.layers.Layer):
-  """Creates a positional embedding.
+class ReLATransformer(Model):
+    def __init__(self,
+                 *,
+                 emb_dim : int,
+                 n_tokens : int,
+                 max_seq_len : int,
+                 depth : int = 4,
+                 causal : bool = True,
+                 n_heads : int = 8,
+                 qkv_dim : int = 64,
+                 expansion_factor : int = 4,
+                 relu_squared : bool = False,
+                 **kwargs):
+        super().__init__(**kwargs)
+        self.depth = depth
 
-  Example:
-  ```python
-  position_embedding = PositionEmbedding(max_length=100)
-  inputs = tf.keras.Input((100, 32), dtype=tf.float32)
-  outputs = position_embedding(inputs)
-  ```
+        self.token_emb = Embedding(n_tokens, emb_dim, name = 'embeddings')
+        self.pos_emb = Embedding(max_seq_len, emb_dim, name = 'embeddings')
 
-
-  Args:
-    max_length: The maximum size of the dynamic sequence.
-    initializer: The initializer to use for the embedding weights. Defaults to
-      "glorot_uniform".
-    seq_axis: The axis of the input tensor where we add the embeddings.
-
-  Reference: This layer creates a positional embedding as described in
-  [BERT: Pre-training of Deep Bidirectional Transformers for Language
-  Understanding](https://arxiv.org/abs/1810.04805).
-  """
-
-  def __init__(self,
-               max_length,
-               initializer="glorot_uniform",
-               seq_axis=1,
-               **kwargs):
-
-    super().__init__(**kwargs)
-    if max_length is None:
-      raise ValueError(
-          "`max_length` must be an Integer, not `None`."
-      )
-    self._max_length = max_length
-    self._initializer = tf.keras.initializers.get(initializer)
-    self._seq_axis = seq_axis
-
-  def build(self, input_shape):
-    dimension_list = input_shape
-    width = dimension_list[-1]
-    weight_sequence_length = self._max_length
-
-    self._position_embeddings = self.add_weight(
-        "embeddings",
-        shape=[weight_sequence_length, width],
-        initializer=self._initializer)
-
-    super().build(input_shape)
-
-  def call(self, inputs):
-    input_shape = tf.shape(inputs)
-    actual_seq_len = input_shape[self._seq_axis]
-    position_embeddings = self._position_embeddings[:actual_seq_len, :]
-    new_shape = [1 for _ in inputs.get_shape().as_list()]
-    new_shape[self._seq_axis] = actual_seq_len
-    new_shape[-1] = position_embeddings.get_shape().as_list()[-1]
-    position_embeddings = tf.reshape(position_embeddings, new_shape)
-    return tf.broadcast_to(position_embeddings, input_shape)
-  
-def FeedForward(dim, mult=4):
-    ff_layer = Sequential()
-    ff_layer.add(LayerNormalization())
-    ff_layer.add(Dense(dim * mult, activation='gelu'))  # Add GELU activation
-    ff_layer.add(Dense(dim))
-
-    return ff_layer
-
-class ReLATransformer(Layer):
-    def __init__(
-        self,
-        *,
-        num_tokens,
-        dim,
-        depth,
-        max_seq_len,
-        causal=True,
-        heads=8,
-        dim_head=64,
-        num_memory_kv=0,
-        no_ff=False,
-        ff_mult=4,
-        relu_squared=False
-    ):
-
-        super().__init__()
-        self.max_seq_len = max_seq_len
-        self.token_emb = Embedding(num_tokens, dim)
-        self.pos_emb = PositionEmbedding(max_seq_len)
-
-        layers = []
-        for i in range(depth):
-            layers.append(ReLA(qkv_dim=dim, relu_squared=relu_squared, n_heads=heads, causal=causal))
-            if not no_ff:
-                layers.append(FeedForward(dim=dim, mult=ff_mult))
-
-        self.layers = Sequential(layers, name='layers')
+        self.blocks = Sequential([
+            Sequential([
+                ReLA(
+                    causal = causal,
+                    qkv_dim = qkv_dim,
+                    n_heads = n_heads,
+                    relu_squared = relu_squared,
+                    name = f'rela{i}'),
+                Sequential([
+                    LayerNormalization(),
+                    Dense(emb_dim * expansion_factor, 'gelu'),
+                    Dense(emb_dim)
+                ], name = f'ffn{i}')
+            ]) for i in range(depth)], name = 'blocks')
 
         self.to_logits = Sequential([
             LayerNormalization(),
-        Dense(num_tokens)  # Define the number of output tokens (n_tokens) here
-        ], name='logits')
+            Dense(n_tokens)
+        ], name = 'logits')
 
-    def call(self, x, mask=None):
-        n, device = x.shape[1], x.device
+    def call(self, x):
         x = self.token_emb(x)
-        x = self.pos_emb(x) + x
-        x = self.layers(x)
+        x = self.pos_emb(tf.range(x.shape[-2]))[tf.newaxis, ...] + x
+        x = self.blocks(x)
         return self.to_logits(x)
